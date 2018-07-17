1b7b5e369deddb386ccd6b218124d64f82b74301
KatoCode23 <Kalen23Imani@gmail.com>
2018-07-16 22:48:36 -0400

    home page added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,29 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            trainees: [
+                {name: 'Sina', progress: 50},
+                {name: 'Tara', progress: 75},
+                {name: 'Eleen', progress: 80},
+                {name: 'Kalen', progress: 40}
+            ]
+
+        };
+    }
+
     render() {
+        const trainees = this.state.trainees.map(
+            (item, index) => {
+                return <li key={index}>{item.name + ' (' + item.progress + '%)'}</li>
+            }  
+        )  
         return (
             <div>
-                <h1>I am home</h1>
+                <h1>Number of trainees: {this.state.trainees.length}</h1>
+                <ul>{trainees}</ul>
                 <Link to="/about">About</Link>
             </div>
         );
