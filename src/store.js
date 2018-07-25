84a928a69cb041b475437447bd4a920b90f6802d
Sina Jazayeri <sin.jaz@gmail.com>
2018-07-24 21:58:10 -0400

    adds caching to the store

--- src/store.js
+++ src/store.js
@@ -8,13 +8,17 @@ let trainees = [];
 
 export async function getData() {
     return new Promise((resolve) => {
-        db = firebase.firestore();
-        db.settings({ timestampsInSnapshots: true });
-        db.collection("trainees").get().then((querySnapshot) => {
-            querySnapshot.forEach((trainee) => {
-                trainees.push(trainee.data());
+        if (trainees.length === 0 ) {   
+            db = firebase.firestore();
+            db.settings({ timestampsInSnapshots: true });
+            db.collection("trainees").get().then((querySnapshot) => {
+                querySnapshot.forEach((trainee) => {
+                    trainees.push(trainee.data());
+                });
+                resolve(trainees);
             });
+        } else {
             resolve(trainees);
-        });
+        }
     })
 }
